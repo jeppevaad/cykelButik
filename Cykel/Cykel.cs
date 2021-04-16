14db14117d6f6f2e5602097fe04acfe6d605b85d
Jeppe Vad <jbva.skp@edu.mercantec.dk>
2021-04-16 23:15:11 +0200

    classes og tests

---
+++ Cykel/Cykel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cykel
+{
+    public class Cykel
+    {
+        public string Navn;
+        public double Pris;
+        public string Type;
+        public int Gear;
+
+        public Cykel(string Navn, double Pris, string Type, int Gear)
+        {
+            this.Navn = Navn;
+            this.Pris = Pris;
+            this.Type = Type;
+            this.Gear = Gear;
+        }
+
+        public List<Cykel> cykelInventar = new List<Cykel>();
+
+        public void opretNyCykel(string navn, double pris, string type, int gear)
+        {
+            cykelInventar.Add(new Cykel(navn, pris, type, gear));
+        }
+        public void sletCykelVedNummerIList(int nummer)
+        {
+            cykelInventar.RemoveAt(nummer);
+        }
+        
+        public void opdaterCykel(int nrIList, string navn, double pris, string type, int gear)
+        {
+            cykelInventar.RemoveAt(nrIList);
+            Cykel opdateretCykel = new Cykel(navn, pris, type, gear);
+            cykelInventar.Insert(nrIList, opdateretCykel);
+        }
+        public Cykel[] HentInventar()
+        {
+            Cykel[] cykelInventar2 = new Cykel[cykelInventar.Count];
+            for (int i = 0; i < cykelInventar2.Length; i++)
+            {
+                cykelInventar2[i] = cykelInventar[i];
+            }
+            return cykelInventar2;
+        }
+
+
+    }
+}
---
+++ CykelTests/CykelTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cykel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Cykel.Tests
+{
+    [TestClass()]
+    public class CykelTests
+    {
+
+
+        [TestMethod()]
+        public void CykelVariablerErSatTest()
+        {
+            // Jeg begyndte at få fejl i unitTest da jeg lavede dette projekt, og kunne ikke få det til at virke ud 'NUnit.Framework.' foran Assert.
+            Cykel nyCykel = new Cykel("cykelnavn", 749, "City Bike", 7);
+            NUnit.Framework.Assert.That(nyCykel.Navn == "cykelnavn");
+            NUnit.Framework.Assert.That(nyCykel.Pris == 749);
+            NUnit.Framework.Assert.That(nyCykel.Type == "City Bike");
+            NUnit.Framework.Assert.That(nyCykel.Gear == 7);
+        }
+
+        [TestMethod()]
+        public void opretNyCykelTest()
+        {
+            Cykel nyCykel = new Cykel("cykelnavn", 749, "City Bike", 7);
+            List<Cykel> cykelInventar = new List<Cykel>();
+            cykelInventar.Add(nyCykel);
+            NUnit.Framework.Assert.That(cykelInventar[0] == nyCykel);
+        }
+
+        [TestMethod()]
+        public void sletCykelTest()
+        {
+            Cykel nyCykel = new Cykel("cykelnavn", 749, "City Bike", 7);
+            Cykel nyCykel2 = new Cykel("BMX", 1000, "BMX", 0);
+            List<Cykel> cykelInventar = new List<Cykel>();
+            cykelInventar.Add(nyCykel);
+            cykelInventar.Add(nyCykel2);
+            //Console.WriteLine(cykelInventar[0].Navn);
+            NUnit.Framework.Assert.That(cykelInventar[0] == nyCykel);
+            cykelInventar.RemoveAt(0);
+            //Console.WriteLine(cykelInventar[0].Navn);
+            NUnit.Framework.Assert.That(cykelInventar[0] != nyCykel);
+        }
+
+        [TestMethod()]
+        public void opdaterCykelTest()
+        {
+            Cykel nyCykel = new Cykel("Dame cykel", 749, "City Bike", 7);
+            Cykel nyCykel2 = new Cykel("BMX", 1000, "BMX", 0);
+            List<Cykel> cykelInventar = new List<Cykel>();
+            cykelInventar.Add(nyCykel);
+            cykelInventar.Add(nyCykel2);
+            NUnit.Framework.Assert.That(cykelInventar[0] == nyCykel);
+            Console.WriteLine(cykelInventar[0].Pris);
+            cykelInventar.RemoveAt(0);
+            Cykel opdateretCykel = new Cykel("Dame cykel", 827, "City Bike", 7);
+            cykelInventar.Insert(0, opdateretCykel);
+            NUnit.Framework.Assert.That(cykelInventar[0].Pris != nyCykel.Pris);
+            Console.WriteLine(cykelInventar[0].Pris);
+        }
+
+        [TestMethod()]
+        public void HentInventarTest()
+        {
+            Cykel nyCykel = new Cykel("Dame cykel", 749, "City Bike", 7);
+            Cykel nyCykel2 = new Cykel("BMX", 1000, "BMX", 0);
+            List<Cykel> cykelInventar = new List<Cykel>();
+            cykelInventar.Add(nyCykel);
+            cykelInventar.Add(nyCykel2);
+
+            Cykel[] cykelInventar2 = new Cykel[cykelInventar.Count];
+            for (int i = 0; i < cykelInventar2.Length; i++)
+            {
+                cykelInventar2[i] = cykelInventar[i];
+            }
+            NUnit.Framework.Assert.That(cykelInventar[0].Pris == cykelInventar2[0].Pris);
+        }
+    }
+}
